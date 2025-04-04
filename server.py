e3a9449f1df8278f69c9ca4c599e705415b1ba02
diegocastanibm <diego.castan@ibm.com>
2025-04-04 14:43:34 -0400

    wip: server

---
+++ server.py
@@ -0,0 +1,26 @@
+# Import Flask, render_template, request from the flask pramework package : TODO
+from flask import Flask, render_template, request
+# Import the sentiment_analyzer function from the package created: TODO
+from EmotionDetection.emotion_detector import emotion_detector
+
+#Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Return a formatted string with the sentiment label and score
+    return "For the given statement, the system response is {}.  The dominant emotion is {}.".format(response, response["dominant_emotion"])
+
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
