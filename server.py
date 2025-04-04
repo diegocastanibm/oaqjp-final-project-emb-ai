e293510d362e1f8941d483499f75b994f31d1e4b
diegocastanibm <diego.castan@ibm.com>
2025-04-04 14:59:20 -0400

    wip: pylint

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Docstring: server.py
+"""
+
 # Import Flask, render_template, request from the flask pramework package : TODO
 from flask import Flask, render_template, request
 # Import the sentiment_analyzer function from the package created: TODO
@@ -8,18 +12,26 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Docstring: sent_anal
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
+
     # Return a formatted string with the sentiment label and score
-    return "For the given statement, the system response is {}.  The dominant emotion is {}.".format(response, response["dominant_emotion"])
+    return f"For the given statement, the system response is {response}. \
+    The dominant emotion is {response['dominant_emotion']}."
 
 
 @app.route("/")
 def render_index_page():
+    """
+    Docstring: render
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
